88bf7734203b7e8b9c2d09e1c6d2df1f69f7d116
Muqsit <hackhack05@gmail.com>
2020-03-03 11:50:01 -0600

    Initial commit

---
+++ src/muqsit/formimagesfix/Main.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace muqsit\formimagesfix;
+
+use Closure;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\event\server\DataPacketReceiveEvent;
+use pocketmine\event\server\DataPacketSendEvent;
+use pocketmine\network\mcpe\protocol\ModalFormRequestPacket;
+use pocketmine\network\mcpe\protocol\NetworkStackLatencyPacket;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+
+final class Main extends PluginBase implements Listener{
+
+	/** @var Closure[][] */
+	private $callbacks = [];
+
+	public function onEnable() : void{
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+
+	private function onPacketSend(Player $player, Closure $callback) : void{
+		$ts = mt_rand() * 1000;
+		$pk = new NetworkStackLatencyPacket();
+		$pk->timestamp = $ts;
+		$pk->needResponse = true;
+		$player->sendDataPacket($pk);
+		$this->callbacks[$player->getId()][$ts] = $callback;
+	}
+
+	/**
+	 * @param DataPacketReceiveEvent $event
+	 * @priority MONITOR
+	 * @ignoreCancelled true
+	 */
+	public function onDataPacketReceive(DataPacketReceiveEvent $event) : void{
+		$packet = $event->getPacket();
+		if($packet instanceof NetworkStackLatencyPacket){
+			$player = $event->getPlayer();
+			if(isset($this->callbacks[$id = $player->getId()][$ts = $packet->timestamp])){
+				$cb = $this->callbacks[$id][$ts];
+				unset($this->callbacks[$id][$ts]);
+				$cb();
+			}
+		}
+	}
+
+	/**
+	 * @param DataPacketSendEvent $event
+	 * @priority MONITOR
+	 * @ignoreCancelled true
+	 */
+	public function onDataPacketSend(DataPacketSendEvent $event) : void{
+		if($event->getPacket() instanceof ModalFormRequestPacket){
+			$player = $event->getPlayer();
+			$this->onPacketSend($event->getPlayer(), static function() use($player) : void{
+				$player->sendMessage("");
+			});
+		}
+	}
+
+	/**
+	 * @param PlayerQuitEvent $event
+	 * @priority MONITOR
+	 */
+	public function onPlayerQuit(PlayerQuitEvent $event) : void{
+		unset($this->callbacks[$event->getPlayer()->getId()]);
+	}
+}
