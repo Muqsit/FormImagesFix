81fbc2e68f32b8cee4438bfcc120b755b6c78b70
Taylor R <noobgamery567@gmail.com>
2023-07-29 22:35:07 +0700

    Reformat

--- src/muqsit/formimagesfix/Main.php
+++ src/muqsit/formimagesfix/Main.php
@@ -16,36 +16,36 @@ use pocketmine\scheduler\ClosureTask;
 
 final class Main extends PluginBase implements Listener{
 
-	protected function onEnable() : void{
-		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-	}
+    protected function onEnable() : void{
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
 
-	/**
-	 * @param DataPacketSendEvent $event
-	 * @priority MONITOR
-	 */
-	public function onDataPacketSend(DataPacketSendEvent $event) : void{
-		foreach($event->getPackets() as $packet){
-			if($packet instanceof ModalFormRequestPacket){
-				foreach($event->getTargets() as $target){
-			                    $player = $target->getPlayer();
-			                    $ts = mt_rand() * 1000;
-			                    $pk = new NetworkStackLatencyPacket();
-			                    $pk->timestamp = $ts;
-			                    $pk->needResponse = true;
-			                    $player->getNetworkSession()->sendDataPacket($pk);
-			                    if($player->isOnline()){
-			                        $times = 5;
-			                        $this->getScheduler()->scheduleRepeatingTask(new ClosureTask(static function() use($player, $target, &$times) : void{
-			                            $entries = [];
-			                            $attr = $player->getAttributeMap()->get(Attribute::EXPERIENCE_LEVEL);
-			                            /** @noinspection NullPointerExceptionInspection */
-			                            $entries[] = new NetworkAttribute($attr->getId(), $attr->getMinValue(), $attr->getMaxValue(), $attr->getValue(), $attr->getDefaultValue(), []);
-			                            $target->sendDataPacket(UpdateAttributesPacket::create($player->getId(), $entries, 0));
-			                        }), 10);
-                    			}
-				}
-			}
-		}
-	}
+    /**
+     * @param DataPacketSendEvent $event
+     * @priority MONITOR
+     */
+    public function onDataPacketSend(DataPacketSendEvent $event) : void{
+        foreach($event->getPackets() as $packet){
+            if($packet instanceof ModalFormRequestPacket){
+                foreach($event->getTargets() as $target){
+                    $player = $target->getPlayer();
+                    $ts = mt_rand() * 1000;
+                    $pk = new NetworkStackLatencyPacket();
+                    $pk->timestamp = $ts;
+                    $pk->needResponse = true;
+                    $player->getNetworkSession()->sendDataPacket($pk);
+                    if($player->isOnline()){
+                        $times = 5;
+                        $this->getScheduler()->scheduleRepeatingTask(new ClosureTask(static function() use($player, $target, &$times) : void{
+                            $entries = [];
+                            $attr = $player->getAttributeMap()->get(Attribute::EXPERIENCE_LEVEL);
+                            /** @noinspection NullPointerExceptionInspection */
+                            $entries[] = new NetworkAttribute($attr->getId(), $attr->getMinValue(), $attr->getMaxValue(), $attr->getValue(), $attr->getDefaultValue(), []);
+                            $target->sendDataPacket(UpdateAttributesPacket::create($player->getId(), $entries, 0));
+                        }), 10);
+                    }
+                }
+            }
+        }
+    }
 }
