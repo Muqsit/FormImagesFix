b71580eb3f541a513205b1a1f0348e97b652485e
Muqsit <hackhack05@gmail.com>
2023-08-05 11:11:41 +0000

    Simplify implementation

--- src/muqsit/formimagesfix/Main.php
+++ src/muqsit/formimagesfix/Main.php
@@ -8,8 +8,6 @@ use pocketmine\entity\Attribute;
 use pocketmine\event\Listener;
 use pocketmine\event\server\DataPacketSendEvent;
 use pocketmine\network\mcpe\protocol\ModalFormRequestPacket;
-use pocketmine\network\mcpe\protocol\types\entity\Attribute as NetworkAttribute;
-use pocketmine\network\mcpe\protocol\UpdateAttributesPacket;
 use pocketmine\plugin\PluginBase;
 use pocketmine\scheduler\CancelTaskException;
 use pocketmine\scheduler\ClosureTask;
@@ -26,27 +24,24 @@ final class Main extends PluginBase implements Listener{
 	 */
 	public function onDataPacketSend(DataPacketSendEvent $event) : void{
 		foreach($event->getPackets() as $packet){
-			if($packet instanceof ModalFormRequestPacket){
-				foreach($event->getTargets() as $target){
-					$this->getScheduler()->scheduleDelayedTask(new ClosureTask(function() use($target) : void{
-						$player = $target->getPlayer();
-						if($player !== null && $player->isOnline()){
-							$times = 5; // send for up to 5 x 10 ticks (or 2500ms)
-							$this->getScheduler()->scheduleRepeatingTask(new ClosureTask(static function() use($player, $target, &$times) : void{
-								if(--$times >= 0 && $target->isConnected()){
-									$entries = [];
-									$attr = $player->getAttributeMap()->get(Attribute::EXPERIENCE_LEVEL);
-									/** @noinspection NullPointerExceptionInspection */
-									$entries[] = new NetworkAttribute($attr->getId(), $attr->getMinValue(), $attr->getMaxValue(), $attr->getValue(), $attr->getDefaultValue(), []);
-									$target->sendDataPacket(UpdateAttributesPacket::create($player->getId(), $entries, 0));
-									return;
-								}
-
-								throw new CancelTaskException("Maximum retries exceeded");
-							}), 10);
-						}
-					}), 1);
+			if(!($packet instanceof ModalFormRequestPacket)){
+				continue;
+			}
+			foreach($event->getTargets() as $target){
+				$player = $target->getPlayer();
+				if($player === null || !$player->isConnected()){
+					continue;
 				}
+				$this->getScheduler()->scheduleDelayedTask(new ClosureTask(function() use($player, $target) : void{
+					$times = 5; // send for up to 5 x 10 ticks (or 2500ms)
+					$this->getScheduler()->scheduleRepeatingTask(new ClosureTask(static function() use($player, $target, &$times) : void{
+						--$times >= 0 || throw new CancelTaskException("Maximum retries exceeded");
+						$target->isConnected() || throw new CancelTaskException("Maximum retries exceeded");
+						$target->getEntityEventBroadcaster()->syncAttributes([$target], $player, [
+							$player->getAttributeMap()->get(Attribute::EXPERIENCE_LEVEL)
+						]);
+					}), 10);
+				}), 1);
 			}
 		}
 	}
